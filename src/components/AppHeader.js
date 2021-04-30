ab8daecc938815fe9dda09ad8db55a5a9ca26d7e
Zarna Nakrani <zarnanakrani18@gnu.ac.in>
2021-04-30 07:49:09 +0000

    Hello

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -3,7 +3,7 @@ import React from 'react';
 function AppHeader() {
 
     return <div>
-        <p className="p-3 text-light h3 bg-primary">Current weather by zip code</p>
+        <p className="p-3 text-light h3 bg-primary">You can enter zip code to see weather update</p>
     </div>
 }
   
